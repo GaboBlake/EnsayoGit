75a1e5f598aaca0915128275642b5dca5a846362
GaboBlake <al44444@un.edu.mx>
2024-06-24 21:15:32 -0600

    agregando lineas

--- EnsayoGit/obj/Debug/net8.0/EnsayoGit.AssemblyInfo.cs
+++ EnsayoGit/obj/Debug/net8.0/EnsayoGit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnsayoGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91114b26acc3735ed46a1a027ad02aefdbf63ae7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb51ff6b579e15d385ae216086a8545aa1a0a1c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnsayoGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnsayoGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
